eb12bdf90df95d880b3e2c23781b598a0565d244
jaenwawe <nwawe.jae@gmail.com>
2020-04-13 20:56:26 -0500

    add readme.md

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//To search run node app query search terms e.g. node app moon walk
-//To add to User List  run node app add #  e.g. node add 0
-
+//1. npm install --save xmlhttprequest
+//2. To search run node app query search terms e.g. node app query moon walk
+//3.  To add to User List  run node app add #  e.g. node app add 0
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const fs = require("fs");
---
+++ readme.md
@@ -0,0 +1,3 @@
+1. npm install --save xmlhttprequest
+2. To search run node app query search terms e.g. node app query moon walk
+3. To add to User List run node app add # e.g. node app add 0
